e0f3050f9176ffc06bdd07643c3c5dadc7fba9b3
Robert M. Avram <robert.avram@rewe-digital.com>
2018-01-03 20:30:17 +0100

    Add tests for mapping and taking

--- test/vector_ops/core_test.clj
+++ test/vector_ops/core_test.clj
@@ -3,12 +3,22 @@
             [clojure.test :refer [is]]
             [clojure.test.check.properties :refer [for-all]]
             [clojure.test.check.generators :as gen]
-            [clojure.test.check.clojure-test :refer [defspec]]))
+            [clojure.test.check.clojure-test :refer [defspec]])
+  (:import (hamt Util)))
 
 
 (defspec concatenation
          (for-all [v1 (gen/vector gen/int)
                    v2 (gen/vector gen/int)]
-                  (is (= (concatv v1 v2) (into v1 v2)))
-                  (is (= v1 v1))
-                  (is (= v2 v2))))
+                  (is (= (concatv v1 v2) (into v1 v2)))))
+
+
+(defspec mapping
+         (for-all [v (gen/vector gen/int)]
+                  (is (= (clojure.core/mapv inc v) (mapv inc v)))))
+
+
+(defspec taking
+         (for-all [v (gen/vector gen/int)
+                   i gen/int]
+                  (is (= (take i v) (takev i v)))))
