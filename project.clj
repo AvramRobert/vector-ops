a5f61b42eea8eb0c0c55e7f49c6f5d17b9d3d400
Robert M. Avram <robert.avram@rewe-digital.com>
2018-01-13 18:45:28 +0100

    Add benchmarks and tests to dev

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["src/java"]
   :main vector-ops.benchmarks
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [criterium "0.4.4"]
-                 [org.clojure/test.check "0.9.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:dependencies [[criterium "0.4.4"]
+                                  [expectations "1.4.41"]
+                                  [org.clojure/test.check "0.9.0"]]}})
