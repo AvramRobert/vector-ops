ee75598ee75e9ce787c364b6223bf9026a620414
Robert M. Avram <robert.avram@rewe-digital.com>
2018-01-27 13:17:58 +0100

    Refactor benchmarks

--- benchmarks/run_benchmarks.sh
+++ benchmarks/run_benchmarks.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-while IFS='' read -r line || [[ -n "$line" ]]; do
-	cd /home/robert/Repositories/vector-ops
-    lein run $line
-done < "$1"
+cd ../
+
+lein run clj-concat
+lein run opt-concat
+lein run clj-map
+lein run opt-map
+lein run clj-take
+lein run opt-take
+lein run clj-drop
+lein run opt-drop
+lein run clj-take-while
+lein run opt-take-while
+lein run clj-drop-while
+lein run opt-drop-while
+lein run clj-take-last-while
+lein run opt-take-last-while
+lein run clj-drop-last-while
+lein run opt-drop-last-while
+lein run clj-split
+lein run opt-split
+lein run clj-slice
+lein run opt-slice
+lein run clj-concat-many
+lein run opt-concat-many
--- src/vector_ops/benchmarks.clj
+++ src/vector_ops/benchmarks.clj
@@ -26,10 +26,11 @@
 
 (defn data-from [v data-type]
   (letfn [(max-min [a]
-            (let [x (rand-nth a)
-                  y (rand-nth a)]
-              (if (= x y) (max-min a) (sort [x y]))))]
+            (let [min (-> (count v) (* 0.2) (int))
+                  max (-> (count v) (* 0.7) (int))]
+              [(nth v min) (nth v max)]))]
     (case data-type
+      :half-val (if (empty? v) 0 (nth v (-> (count v) (/ 2) (int))))
       :rand-val (if (empty? v) 0 (rand-nth v))
       :range    (cond (empty? v) [0 0]
                       (= 1 (count v)) [(first v) (first v)]
@@ -83,109 +84,109 @@
   (run-benchmark {:name "clj-map"
                   :ranges data-ranges
                   :data-type :rand-val
-                  :expr (fn [data n] (mapv #(+ % n) data))}))
+                  :expr (fn [data _] (mapv inc data))}))
 
 (defn bench-mapv-opt []
   (run-benchmark {:name "opt-map"
                   :ranges data-ranges
                   :data-type :rand-val
-                  :expr (fn [data n] (o/mapv #(+ % n) data))}))
+                  :expr (fn [data _] (o/mapv inc data))}))
 
 (defn bench-take-clj []
   (run-benchmark {:name "clj-take"
                   :ranges data-ranges
-                  :data-type :rand-val
+                  :data-type :half-val
                   :expr (fn [data n] (vec (take n data)))}))
 
 (defn bench-take-opt []
   (run-benchmark {:name "opt-take"
                   :ranges data-ranges
-                  :data-type :rand-val
+                  :data-type :half-val
                   :expr (fn [data n] (o/takev n data))}))
 
 (defn bench-drop-clj []
   (run-benchmark {:name "clj-drop"
                   :ranges data-ranges
-                  :data-type :rand-val
+                  :data-type :half-val
                   :expr (fn [data n] (vec (drop n data)))}))
 
 (defn bench-drop-opt []
   (run-benchmark {:name "opt-drop"
                   :ranges data-ranges
-                  :data-type :rand-val
+                  :data-type :half-val
                   :expr (fn [data n] (o/dropv n data))}))
 
 (defn bench-split-clj []
   (run-benchmark {:name "clj-split"
                   :ranges data-ranges
-                  :data-type :rand-val
+                  :data-type :half-val
                   :expr (fn [data n] (mapv vec (split-at n data)))}))
 
 (defn bench-split-opt []
   (run-benchmark {:name "opt-split"
                   :ranges data-ranges
-                  :data-type :rand-val
+                  :data-type :half-val
                   :expr (fn [data n] (o/splitv-at n data))}))
 
 (defn bench-take-while-clj []
   (run-benchmark {:name "clj-take-while"
                   :ranges data-ranges
-                  :data-type :rand-val
+                  :data-type :half-val
                   :expr (fn [data n] (vec (take-while #(< % n) data)))}))
 
 (defn bench-take-while-opt []
   (run-benchmark {:name "opt-take-while"
                   :ranges data-ranges
-                  :data-type :rand-val
+                  :data-type :half-val
                   :expr (fn [data n] (o/takev-while #(< % n) data))}))
 
 (defn bench-drop-while-clj []
   (run-benchmark {:name "clj-drop-while"
                   :ranges data-ranges
-                  :data-type :rand-val
+                  :data-type :half-val
                   :expr (fn [data n] (vec (drop-while #(< % n) data)))}))
 
 (defn bench-drop-while-opt []
   (run-benchmark {:name "opt-drop-while"
                   :ranges data-ranges
-                  :data-type :rand-val
+                  :data-type :half-val
                   :expr (fn [data n] (o/dropv-while #(< % n) data))}))
 
 (defn bench-take-last-while-clj [] []
   (run-benchmark {:name "clj-take-last-while"
                   :ranges data-ranges
-                  :data-type :rand-val
+                  :data-type :half-val
                   :expr (fn [data n] (->> (reverse data) (take-while #(> % n)) (reverse)))}))
 
 (defn bench-take-last-while-opt [] []
   (run-benchmark {:name "opt-take-last-while"
                   :ranges data-ranges
-                  :data-type :rand-val
+                  :data-type :half-val
                   :expr (fn [data n] (o/takev-last-while #(> % n) data))}))
 
 (defn bench-drop-last-while-clj []
   (run-benchmark {:name "clj-drop-last-while"
                   :ranges data-ranges
-                  :data-type :rand-val
+                  :data-type :half-val
                   :expr (fn [data n] (->> (reverse data) (drop-while #(> % n)) (reverse)))}))
 
 (defn bench-drop-last-while-opt []
   (run-benchmark {:name "opt-drop-last-while"
                   :ranges data-ranges
-                  :data-type :rand-val
+                  :data-type :half-val
                   :expr (fn [data n] (o/dropv-last-while #(> % n) data))}))
 
 (defn bench-slice-clj []
   (run-benchmark {:name "clj-slice"
                   :ranges data-ranges
                   :data-type :range
-                  :expr (fn [data [min max]] (->> data (take max) (drop min) (vec)))}))
+                  :expr (fn [data [mn mx]] (->> data (take mx) (drop mn) (vec)))}))
 
 (defn bench-slice-opt []
   (run-benchmark {:name "opt-slice"
-                  :ranges data-ranges
+                  :ranges [100 500]                                   ;data-ranges
                   :data-type :range
-                  :expr (fn [data [min max]] (o/slicev data min max))}))
+                  :expr (fn [data [mn mx]] (o/slicev data mn mx))}))
 
 (defn -main [& args]
   (case (keyword (first args))
@@ -210,5 +211,5 @@
     :clj-slice           (bench-slice-clj)
     :opt-slice           (bench-slice-opt)
     :clj-concat-many     (bench-concat-many-clj)
-    :opt-concat-many     (bench-concat-many-opt))
-    (println "Benchmark not specified. Nothing to benchmark."))
+    :opt-concat-many     (bench-concat-many-opt)
+    (println "Benchmark not specified. Nothing to benchmark.")))
