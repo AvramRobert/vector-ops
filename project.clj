aa01a3f2dadd9fec6ad81e624e66ea51c9d57a14
Robert Marius Avram <am11.robert@yahoo.com>
2018-02-03 23:57:50 +0100

    Add publishing

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject vector-ops "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Optimised vector operations"
+  :url "https://github.com/AvramRobert/vector-ops"
   :license {:name "Apache-2.0 License"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :java-source-paths ["src/java"]
   :main vector-ops.benchmarks.core
   :dependencies [[org.clojure/clojure "1.8.0"]]
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :cred :gpg}]]
   :profiles {:dev {:dependencies [[criterium "0.4.4"]
                                   [expectations "1.4.41"]
                                   [org.clojure/test.check "0.9.0"]]}})
